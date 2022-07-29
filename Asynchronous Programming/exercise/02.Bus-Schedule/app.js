a34dc744acd0262b85f83c154c08fa959347f892
Ivan <vankata294n4@gmail.com>
2022-07-29 18:46:57 +0300

    Solved Bus-Schedule exercise

--- Asynchronous Programming/exercise/02.Bus-Schedule/app.js
+++ Asynchronous Programming/exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,37 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const infoLabel = document.querySelector('.info');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive')
+    console.log(arriveBtn)
+
+    let busData = {
+        next: "depot",
+        name: "",
+    }
+
+    async function depart() {
+        
+        try {
+            departBtn.disabled = true;
+            const res = await fetch('http://localhost:3030/jsonstore/bus/schedule/' + busData.next)
+            const data = await res.json()
+            
+            busData = data;
+            infoLabel.textContent = 'Next stop ' + busData.name;
+            arriveBtn.disabled = false;
+        } catch (err) {
+            infoLabel.textContent = 'ERROR';
+            arriveBtn.disabled = true;
+            departBtn.disabled = true;
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
+
+        infoLabel.textContent = `Arriving at ${busData.name}`
     }
 
     return {
