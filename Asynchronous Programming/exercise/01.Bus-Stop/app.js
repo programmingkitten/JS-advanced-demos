15683dc5a9b52cd3f71a555a849682f1737c953e
Ivan <vankata294n4@gmail.com>
2022-07-29 18:29:00 +0300

    Solved bus-stop exercise

--- Asynchronous Programming/exercise/01.Bus-Stop/app.js
+++ Asynchronous Programming/exercise/01.Bus-Stop/app.js
@@ -1,3 +1,38 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    
+
+
+    const input = document.getElementById('stopId')
+    const textDiv = document.getElementById('stopName')
+    textDiv.innerHTML = ""
+    let busId = input.value
+    const textArea = document.createElement('p')
+    
+    textDiv.appendChild(textArea)
+
+    try {
+    const res = await fetch(`http://localhost:3030/jsonstore/bus/businfo/` + busId)
+    const data = await res.json()
+    let bussData = Object.entries(
+        JSON.parse(
+        JSON.stringify(data, null, 2)
+        ));
+
+    console.log(Object.entries(bussData))
+    textArea.textContent = bussData[1][1]
+
+    // const busGraphic = document.createElement('li')
+
+    busGraphic = bussData[0][1]
+
+    const bussUl = document.getElementById('buses')
+    bussUl.innerHTML = '';
+    for (let [id, time] of Object.entries(busGraphic)) {
+        const currentBussInfo = document.createElement('li')
+        currentBussInfo.textContent = `Bus ${id} arrives in ${time} minutes`
+        bussUl.appendChild(currentBussInfo)
+    }
+    } catch (err) {
+        textArea.textContent = "ERROR"
+    }
 }
